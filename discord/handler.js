a81bd4f06766975bd3cb2636508674bfec883702
juvian <juliandelgobbo@gmail.com>
2020-09-22 01:46:49 -0300

    adding tests

--- discord/handler.js
+++ discord/handler.js
@@ -1,18 +1,29 @@
 class Handler {
     handlers = [];
-
+    
+    constructor(...handlers) {
+        this.add(...handlers);
+    }
+    
     add(...handlers) {
         this.handlers.push(handlers);
     }
 
 
     async runHandlers(handler, context, idx = 0) {
-        if (idx >= handler.length) return true;
-        if (handler[idx] instanceof Handler) return handler[idx].run(context);
+        if (idx >= handler.length) {
+            context.done = true; return;
+        }
+
+        if (handler[idx] instanceof Handler) {
+            let res = await handler[idx].run(context);
+            if (res && res.done) return res;
+            return this.runHandlers(handler, context, idx + 1);
+        }
 
         let func = handler[idx].run || handler[idx];
 
-        func(context, () => this.runHandlers(handler, context, idx));
+        func(context, () => this.runHandlers(handler, context, idx + 1));
     }
 
 
@@ -20,10 +31,10 @@ class Handler {
         if (idx >= this.handlers.length) return;
         let copy = Object.assign({}, context);
         
-        let done = await this.runHandlers(this.handlers[idx], copy);
-        console.log(done)
-        if (done === true) return copy;
-        else await this.run(context, idx + 1);    
+        await this.runHandlers(this.handlers[idx], copy);
+        console.log(copy)
+        if (copy.done === true) return copy;
+        else return this.run(context, idx + 1);    
     }
 }
 
--- tests/handler.js
+++ tests/handler.js
@@ -2,12 +2,30 @@ const Handler = require('../discord/handler');
 const {assertEquals} = require('./tests');
 
 let count = (context, next) => {context.count++; next()}; 
+let noop = () => {}
+
+async function assertHandler(handler, context, result) {
+    let res = await handler.run(context);
+    assertEquals(JSON.stringify(res), JSON.stringify(result));
+}
 
 async function test() {
-    let handler = new Handler();
-    handler.add(count, count);
-    let result = await handler.run({count: 0});
-    assertEquals(result.count, 2);
+    let handler = new Handler(count, count);
+    await assertHandler(handler, {count: 0}, {count: 2, done: true});
+}
+
+async function testNested() {
+    let h = new Handler();
+    let handler = new Handler(count, h, count);
+    await assertHandler(handler, {count: 0}, {count: 2, done: true});
+    h.add(count, noop, count); // doesnt finish
+    await assertHandler(handler, {count: 0}, null);
+    h.add(count, count, count, noop, count); // doesnt finish
+    await assertHandler(handler, {count: 0}, null);
+
+    handler.add(count);
+    await assertHandler(handler, {count: 0}, {count: 1, done: true});
 }
 
 test();
+testNested();
